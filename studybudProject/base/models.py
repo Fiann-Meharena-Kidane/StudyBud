e1455efe6bc2f1b4c566c18056ee1e8301ebabe7
finan <finanmeharenakidane@gmail.com>
2022-10-02 05:10:37 +0400

    tempaltes | include, extends, modeling | admin page,

--- studybudProject/base/models.py
+++ studybudProject/base/models.py
@@ -1,3 +1,19 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host
+    # topic
+    name=models.CharField(max_length=200)
+    description=models.TextField(null=True, blank=True)
+    # participants=
+    created=models.DateTimeField(auto_now_add=True)
+    # updated=model.models.DateTimeField(auto_now=True)
+    
+    
+    
+    def __str__(self):
+        return self.name 
--- studybudProject/base/views.py
+++ studybudProject/base/views.py
@@ -1,11 +1,18 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 
-def home(request):
-    return HttpResponse('Home Page')
 
-def room(request):
-    return HttpResponse("this is my room ")
+
+def home(request):
+    return render(request, 'base/home.html')
+
+
+def room(request, name):
+    
+    context={'name':name}
+    return render(request, 'base/room.html', context)
