169b1b514a6a625e112e7155d071659651d68f43
finan <finanmeharenakidane@gmail.com>
2022-10-04 09:39:13 +0400

    added form part I

--- studybudProject/base/views.py
+++ studybudProject/base/views.py
@@ -1,7 +1,8 @@
 from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -15,3 +16,13 @@ def room(request, name):
     
     context={'name':name}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form=RoomForm()
+    # if request.method=='POST':
+    #     form=RoomForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return 'hello'
+    context={'form':form}
+    return render(request, 'base/room-form.html', context)
